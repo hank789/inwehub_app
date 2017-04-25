075144f8eccdfd99da7aa842ba4006ea2a969010
hank <hank.huiwang@gmail.com>
2017-04-25 17:17:52 +0800

    回答增加回答时间

--- app/Api/Controllers/Ask/AnswerController.php
+++ app/Api/Controllers/Ask/AnswerController.php
@@ -93,6 +93,7 @@ class AnswerController extends Controller
                 $question->answered();
 
                 $answer->status = 1;
+                $answer->adopted_at = date('Y-m-d H:i:s');
                 $answer->save();
 
                 UserTag::multiIncrement($loginUser->id,$question->tags()->get(),'answers');
