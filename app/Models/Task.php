32d4bd0b9b89283901152b4a01c452ad2268e0eb
hank <hank.huiwang@gmail.com>
2017-04-28 15:24:20 +0800

    优化

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -14,7 +14,7 @@ class Task extends Model
 {
     use BelongsToUserTrait;
     protected $table = 'task';
-    protected $fillable = ['user_id', 'source_type','source_id','subject','status'];
+    protected $fillable = ['user_id', 'source_type','source_id','subject','status','action'];
 
     const ACTION_TYPE_ANSWER = 'answer';
     const ACTION_TYPE_ANSWER_FEEDBACK = 'answer_feedback';
