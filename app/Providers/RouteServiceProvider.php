4df44418720d7b82a9aed604af36a8b024a96b08
hank <hank.huiwang@gmail.com>
2017-05-04 18:19:49 +0800

    优化路由

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -80,10 +80,10 @@ class RouteServiceProvider extends ServiceProvider
      */
     protected function mapApiRoutes()
     {
-        Route::prefix('api')
-             ->middleware('api')
-             ->namespace($this->api_namespace)
-             ->group(base_path('routes/api.php'));
+        Route::middleware('api')
+            ->prefix('api')
+            ->namespace($this->api_namespace)
+            ->group(base_path('routes/api.php'));
     }
 
     protected function mapVendorRoutes()
