a9edb1b2d4b7133bcaa05c62dc5bbf52ea12f8fa
hank <hank.huiwang@gmail.com>
2017-05-18 17:35:49 +0800

    版本更新机制

--- app/Api/Controllers/SystemController.php
+++ app/Api/Controllers/SystemController.php
@@ -51,10 +51,12 @@ class SystemController extends Controller {
         $android_version = Setting()->get('android_version','1.0.0');
         $is_force = Setting()->get('is_force','0');
         $update_msg = Setting()->get('update_msg','1、大额提现t+1到账。\n2、变现进度做了优化。\n3、修复了一些bug。');
+        $package_url = 'http://demo.dcloud.net.cn/test/update/H5EF3C469.wgt';
         return self::createJsonData(true,[
             'ios_version'           => $ios_version,
             'android_version'       => $android_version,
             'is_force'              => $is_force,
+            'package_url'           => $package_url,
             'update_msg'            => $update_msg
         ]);
     }
