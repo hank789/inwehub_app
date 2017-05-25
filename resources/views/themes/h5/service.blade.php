2cceded732a410a2b89e55b8a68a6276f4301ad7
hank <hank.huiwang@gmail.com>
2017-05-25 21:09:52 +0800

    模板优化

--- resources/views/themes/h5/service.blade.php
+++ resources/views/themes/h5/service.blade.php
@@ -1,47 +1,10 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <meta name="viewport" content="width=device-width,height=device-height,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no">
-    <title>服务条款</title>
-    <style>
-        html, body {
-            height: 100%;
-        }
-        body {
-            margin: 0;
-            width: 100%;
-            display: table;
-            /*font-weight: 100;*/
-            font-family: 'Lato';
-            color: #333;
-        }
-        .container {
-            margin-top: 20px;
-            vertical-align: middle;
-        }
-        .content {
-            padding: 70px 0 170px;
-            margin: 0 auto;
-            width: 1100px;
-            position: relative;
-        }
-        .content p,h3,h4{
-            text-indent: 32px;
-            font-weight: 400;
-            line-height: 32px;
-            font-size: 18px;
-            border: none;
-            margin: 0;
-            padding: 0;
-        }
+@extends('h5::layout')
 
-    </style>
-</head>
-<body>
-<div class="container">
-    <div class="content">
-        {!! Setting()->get('register_license','') !!}
-    </div>
-</div>
-</body>
-</html>
+@section('title') 服务条款 @endsection
+
+
+@section('content')
+
+    {!! Setting()->get('register_license','') !!}
+
+@endsection
