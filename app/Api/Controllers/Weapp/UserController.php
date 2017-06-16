5d46bebf7df2ba13a217379b60993ea5d48e98c0
hank <hank.huiwang@gmail.com>
2017-06-16 16:14:17 +0800

    debug

--- app/Api/Controllers/Weapp/UserController.php
+++ app/Api/Controllers/Weapp/UserController.php
@@ -6,6 +6,7 @@
  */
 use App\Api\Controllers\Controller;
 use App\Third\Weapp\Wxxcx;
+use Illuminate\Http\Request;
 
 class UserController extends controller {
     protected $wxxcx;
@@ -21,18 +22,23 @@ class UserController extends controller {
      * @date   2017-05-27T14:37:08+0800
      * @return [type]                   [description]
      */
-    public function getWxUserInfo()
+    public function getWxUserInfo(Request $request)
     {
         //code 在小程序端使用 wx.login 获取
         $code = request('code', '');
         //encryptedData 和 iv 在小程序端使用 wx.getUserInfo 获取
         $encryptedData = request('encryptedData', '');
         $iv = request('iv', '');
+        \Log::info('test',$request->all());
 
+        \Log::info('testtest',[$code,$encryptedData,$iv]);
         //根据 code 获取用户 session_key 等信息, 返回用户openid 和 session_key
         $userInfo = $this->wxxcx->getLoginInfo($code);
 
+        \Log::info('userinfo',[$userInfo]);
         //获取解密后的用户信息
-        return $this->wxxcx->getUserInfo($encryptedData, $iv);
+        $return = $this->wxxcx->getUserInfo($encryptedData, $iv);
+        \Log::info('return',[$return]);
+        return $return;
     }
 }
