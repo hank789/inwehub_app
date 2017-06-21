3fb0924a2ab5983cbbb824959970f84e5dfd2d3e
hank <hank.huiwang@gmail.com>
2017-06-21 21:04:10 +0800

    update

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -15,6 +15,7 @@ class Kernel extends ConsoleKernel
     protected $commands = [
         Commands\Test::class,
         Commands\Pay\Settlement::class,
+        Commands\Wechat\AddMenu::class,
     ];
 
     /**
