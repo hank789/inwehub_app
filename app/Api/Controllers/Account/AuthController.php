46e8bde2136d5ba364ee53ffa5757863259a0d7d
hank <hank.huiwang@gmail.com>
2017-06-27 20:22:51 +0800

    update

--- app/Api/Controllers/Account/AuthController.php
+++ app/Api/Controllers/Account/AuthController.php
@@ -286,7 +286,7 @@ class AuthController extends Controller
         }
 
         //验证手机验证码
-        $code_cache = Cache::get(SendPhoneMessage::getCacheKey('register',$mobile));
+        $code_cache = Cache::get(SendPhoneMessage::getCacheKey('wx_gzh_register',$mobile));
         $code = $request->input('code');
         if($code_cache != $code){
             throw new ApiException(ApiException::ARGS_YZM_ERROR);
