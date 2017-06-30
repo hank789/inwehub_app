55fbba4c1691948a4ab57a986ac3a679a33bdfed
hank <hank.huiwang@gmail.com>
2017-06-30 18:15:23 +0800

    微信菜单修改

--- app/Console/Commands/Wechat/AddMenu.php
+++ app/Console/Commands/Wechat/AddMenu.php
@@ -33,7 +33,7 @@ class AddMenu extends Command
         $wechat = app('wechat');
         $menu = $wechat->menu;
         $url = config('app.url');
-        $buttons = [
+        /*$buttons = [
             [
                 "name"       => "平台服务",
                 "sub_button" => [
@@ -67,6 +67,28 @@ class AddMenu extends Command
                         "name" => "公司官网",
                         "url"  => "https://www.inwehub.com"
                     ]
+                ],
+            ],
+        ];*/
+        $buttons = [
+            [
+                "type" => "view",
+                "name" => "进入平台",
+                "url"  => $url."/wechat/oauth?redirect=/home"
+            ],
+            [
+                "name"       => "更多信息",
+                "sub_button" => [
+                    [
+                        "type" => "view",
+                        "name" => "产品官网",
+                        "url"  => "https://www.inwehub.com"
+                    ],
+                    [
+                        "type" => "view",
+                        "name" => "推荐专家",
+                        "url" => "http://cn.mikecrm.com/tgx3vq8"
+                    ],
                 ],
             ],
         ];
