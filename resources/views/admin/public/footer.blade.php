8808e07143746e37420bd2bb7c15eca57c5e7e49
hank <hank.huiwang@gmail.com>
2017-07-01 22:37:51 +0800

    后台区分环境

--- resources/views/admin/public/footer.blade.php
+++ resources/views/admin/public/footer.blade.php
@@ -1,3 +1,21 @@
 <footer class="main-footer text-center">
     <strong>Copyright &copy; 2010-{{ date('Y') }} <a href="http://www.inwehub.com">inwehub.com</a>.</strong> All rights reserved.
 </footer>
+<div class="post-button">{{ config('app.env') == 'production' ? '正式环境':'测试环境' }}</div>
+<style>
+    .post-button {
+        position: fixed;
+        top: 10px;
+        right: 50px;
+        width:300px;
+        line-height:38px;
+        text-align:center;
+        background:red;
+        color:#fff;
+        -webkit-border-radius:100px;
+        border-radisu:100px;
+        left:50%;
+        margin-left:-150px;
+        z-index:1040;
+    }
+</style>
