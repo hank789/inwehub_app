51c6e83b6fb4c9223c2a5a78c552e3ebaba7d210
hank <hank.huiwang@gmail.com>
2017-07-25 17:19:53 +0800

    优化

--- app/helpers.php
+++ app/helpers.php
@@ -85,6 +85,7 @@ if (! function_exists('trans_company_apply_status')) {
 
     function trans_company_apply_status($status){
         $map = [
+            0 => '草稿',
             1 => '待审核',
             2 => '审核通过',
             3 => '审核失败',
