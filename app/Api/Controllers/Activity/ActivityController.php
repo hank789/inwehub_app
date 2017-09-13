c2982d36698d00d787f086d63f8d19fdf82f64d4
hank <hank.huiwang@gmail.com>
2017-09-13 17:48:55 +0800

    优化活动列表

--- app/Api/Controllers/Activity/ActivityController.php
+++ app/Api/Controllers/Activity/ActivityController.php
@@ -42,10 +42,10 @@ class ActivityController extends Controller {
         $is_mine = $request->input('is_mine');
 
         if ($is_mine) {
-            $articles = Collection::where('collections.user_id',$request->user()->id)->where('collections.source_type','App\Models\Article')->leftJoin('articles','collections.source_id','=','articles.id')->select('articles.*','collections.status as c_status')->orderBy('articles.id','DESC')->paginate(10);
+            $articles = Collection::where('collections.user_id',$request->user()->id)->where('articles.category_id',$category->id)->where('collections.source_type','App\Models\Article')->leftJoin('articles','collections.source_id','=','articles.id')->select('articles.*','collections.status as c_status')->orderBy('articles.id','DESC')->paginate(10);
             $return = $articles->toArray();
         } else {
-            $articles = Article::where('articles.status','>',0)->where('collections.source_type','App\Models\Article')->leftJoin('collections','articles.id','=','collections.source_id')->select('articles.*','collections.status as c_status')->orderBy('articles.id','DESC')->paginate(10);
+            $articles = Article::where('articles.category_id',$category->id)->where('articles.status','>',0)->where('collections.source_type','App\Models\Article')->leftJoin('collections','articles.id','=','collections.source_id')->select('articles.*','collections.status as c_status')->orderBy('articles.id','DESC')->paginate(10);
             $return = $articles->toArray();
         }
 
