657da984149f94fa30afe88d20cdae5c63eae453
hank <hank.huiwang@gmail.com>
2017-10-19 19:01:34 +0800

    stash

---
+++ app/Notifications/FollwedUserAnswered.php
@@ -0,0 +1,128 @@
+<?php
+
+namespace App\Notifications;
+
+use App\Channels\PushChannel;
+use App\Channels\WechatNoticeChannel;
+use App\Models\Answer;
+use App\Models\Notification as NotificationModel;
+use App\Models\Question;
+use Illuminate\Bus\Queueable;
+use Illuminate\Notifications\Notification;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+class FollwedUserAnswered extends Notification implements ShouldBroadcast,ShouldQueue
+{
+    use Queueable,InteractsWithSockets;
+
+    protected $question;
+    protected $answer;
+    protected $user_id;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public function __construct($user_id, Question $question, Answer $answer)
+    {
+        $this->user_id = $user_id;
+        $this->question = $question;
+        $this->answer = $answer;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['database', 'broadcast', PushChannel::class, WechatNoticeChannel::class];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        return (new MailMessage)
+                    ->line('The introduction to the notification.')
+                    ->action('Notification Action', url('/'))
+                    ->line('Thank you for using our application!');
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        switch ($this->question->question_type) {
+            case 1:
+                $url = '/ask/'.$this->question->id;
+                $title = '专家';
+                break;
+            case 2:
+                $url = '/askCommunity/interaction/'.$this->answer->id;
+                $title = '用户';
+                break;
+        }
+        return [
+            'url'    => $url,
+            'notification_type' => NotificationModel::NOTIFICATION_TYPE_TASK,
+            'avatar' => $this->answer->user->avatar,
+            'title'  => '您好，'.$title.$this->answer->user->name.'回答了您关注的问题',
+            'body'   => $this->question->title,
+            'extra_body' => ''
+        ];
+    }
+
+    public function toPush($notifiable)
+    {
+        switch ($this->question->question_type) {
+            case 1:
+                $object_id = $this->question->id;
+                $object_type = 'pay_question_answered';
+                break;
+            case 2:
+                $object_id = $this->answer->id;
+                $object_type = 'free_question_answered';
+                break;
+        }
+        return [
+            'title' => '用户'.$this->answer->user->name.'回答了您关注的问题',
+            'body'  => $this->question->title,
+            'payload' => ['object_type'=>$object_type,'object_id'=>$object_id],
+        ];
+    }
+
+    public function toWechatNotice($notifiable){
+        switch ($this->question->question_type) {
+            case 1:
+                $object_type = 'followed_pay_question_answered';
+                break;
+            case 2:
+                $object_type = 'followed_free_question_answered';
+                break;
+        }
+        return [
+            'keyword1' => $this->question->title,
+            'object_type'  => $object_type,
+            'object_id' => $this->answer->id,
+        ];
+    }
+
+    public function broadcastOn(){
+        return ['notification.user.'.$this->user_id];
+    }
+}
