e33fb378dd4085a283b56fe0199a1e15c6a97b37
hank <hank.huiwang@gmail.com>
2017-11-21 11:20:22 +0800

    优化

--- resources/views/admin/operate/article/index.blade.php
+++ resources/views/admin/operate/article/index.blade.php
@@ -53,7 +53,7 @@
                                         <tr>
                                             <td><input type="checkbox" value="{{ $submission->id }}" name="ids[]"/></td>
                                             <td>{{ $submission->id }}</td>
-                                            <td><a href="{{ config('app.mobile_url').'#/c/'.$submission->data['category_id'].'/'.$submission->data['slug'] }}" target="_blank">{{ $submission->title }}</a></td>
+                                            <td><a href="{{ config('app.mobile_url').'#/c/'.$submission->category_id.'/'.$submission->slug }}" target="_blank">{{ $submission->title }}</a></td>
                                             <td>{{ $submission->category_name }}</td>
                                             <td>{{ $submission->owner->name }}</td>
                                             <td>{{ $submission->updated_at }}</td>
