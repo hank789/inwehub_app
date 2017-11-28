2373646827fca28b646a4304b7dba18cb52214cf
hank <hank.huiwang@gmail.com>
2017-11-28 12:17:15 +0800

    企业用户数据初始化

--- app/Console/Commands/FixData/CompanyData.php
+++ app/Console/Commands/FixData/CompanyData.php
@@ -5,7 +5,12 @@
  * @email: wanghui@yonglibao.com
  */
 
+use App\Models\Company\CompanyDataUser;
+use App\Models\User;
+use App\Models\UserInfo\JobInfo;
+use App\Models\UserInfo\ProjectInfo;
 use Illuminate\Console\Command;
+use App\Models\Company\CompanyData as CompanyDataModel;
 
 class CompanyData extends Command
 {
@@ -30,7 +35,51 @@ class CompanyData extends Command
      */
     public function handle()
     {
+        $jobs = JobInfo::get();
+        $users = User::get();
+        foreach ($users as $user) {
+            $this->addData($user->company,$user->id,1);
+        }
+        foreach ($jobs as $job) {
+            $this->addData($job->company,$job->user_id,$job->end_time == '至今' ? 1:3);
+        }
 
+        $projects = ProjectInfo::get();
+        foreach ($projects as $project) {
+            $this->addData($project->customer_name,$project->user_id,2);
+        }
     }
+
+    protected function addData($companyName,$user_id,$userCompanyStatus){
+        $exist = CompanyDataModel::where('name',$companyName)->first();
+        if (!$exist) {
+            $data = CompanyDataModel::create([
+                'name' => $companyName,
+                'logo' => '',
+                'address_province' => '',
+                'address_detail'   => '',
+                'longitude'        => '',
+                'latitude'         => '',
+                'audit_status'     => 0
+            ]);
+            CompanyDataUser::create([
+                'company_data_id' => $data->id,
+                'user_id'         => $user_id,
+                'audit_status'    => 0,
+                'status'          => $userCompanyStatus
+            ]);
+        } else {
+            $existUser = CompanyDataUser::where('company_data_id',$exist->id)->where('user_id',$user_id)->first();
+            if (!$existUser) {
+                CompanyDataUser::create([
+                    'company_data_id' => $exist->id,
+                    'user_id'         => $user_id,
+                    'audit_status'    => 0,
+                    'status'          => $userCompanyStatus
+                ]);
+            }
+        }
+    }
+
 
 }
