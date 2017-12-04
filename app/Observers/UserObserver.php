5cef8964c5a15b20685721c0aae177361febb6ff
hank <hank.huiwang@gmail.com>
2017-12-04 15:47:37 +0800

    优化

--- app/Observers/UserObserver.php
+++ app/Observers/UserObserver.php
@@ -7,6 +7,7 @@
 
 use App\Cache\UserCache;
 use App\Models\Company\CompanyData;
+use App\Models\Company\CompanyDataUser;
 use App\Models\Readhub\ReadHubUser;
 use App\Models\User;
 use Illuminate\Contracts\Queue\ShouldQueue;
@@ -23,6 +24,8 @@ class UserObserver implements ShouldQueue {
     public function updated(User $user){
         UserCache::delUserInfoCache($user->id);
         //同步公司信息
+        //先将之前公司状态改为离职
+        CompanyDataUser::where('user_id',$user->id)->where('status',1)->update(['status'=>3]);
         CompanyData::initCompanyData($user->company,$user->id,1);
     }
 
