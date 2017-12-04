dcd03c8984f31c977853e136a060a03f8da49e20
hank <hank.huiwang@gmail.com>
2017-12-04 15:39:59 +0800

    优化

--- app/Models/Company/CompanyData.php
+++ app/Models/Company/CompanyData.php
@@ -104,15 +104,19 @@ class CompanyData extends Model
             if (count($city['result']) >= 1) {
                 $object1 = $city['result'][0];
                 $address_province = $object1['city'].$object1['district'];
-                $longitude = $object1['location']['lng'];
-                $latitude = $object1['location']['lat'];
+                if (isset($object1['location'])) {
+                    $longitude = $object1['location']['lng'];
+                    $latitude = $object1['location']['lat'];
+                }
             }
             if ($location['total'] >= 1) {
                 $object2 = $location['results'][0];
                 if (isset($object2['address'])) {
                     $address_detail = $object2['address'];
-                    $longitude = $object2['detail_info']['navi_location']['lng'];
-                    $latitude = $object2['detail_info']['navi_location']['lat'];
+                    if (isset($object2['detail_info']['navi_location'])) {
+                        $longitude = $object2['detail_info']['navi_location']['lng'];
+                        $latitude = $object2['detail_info']['navi_location']['lat'];
+                    }
                 }
             }
             if ($longitude) {
