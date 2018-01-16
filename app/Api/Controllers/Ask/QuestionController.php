e235f4cdb3415e32292df426052b2d68a27028ea
hank <hank.huiwang@gmail.com>
2018-01-16 16:37:11 +0800

    优化

--- app/Api/Controllers/Ask/QuestionController.php
+++ app/Api/Controllers/Ask/QuestionController.php
@@ -595,8 +595,8 @@ class QuestionController extends Controller
         $invitedUsers = $question->invitations()->where("from_user_id","=",$request->user()->id)->pluck('user_id')->toArray();
         $invitedUsers[] = $question->user_id;
         $invitedUsers[] = $request->user()->id;
-        $query = UserTag::select(['user_id','skills','answers']);
-        $query1 = UserTag::select(['user_id','skills','answers']);
+        $query = UserTag::select('user_id')->orderBy('skills','desc')->orderBy('answers','desc')->distinct();
+        $query1 = UserTag::select('user_id')->orderBy('skills','desc')->orderBy('answers','desc')->distinct();
         if ($invitedUsers) {
             $query = $query->whereNotIn('user_id',$invitedUsers);
             $query1 = $query1->whereNotIn('user_id',$invitedUsers);
@@ -606,7 +606,7 @@ class QuestionController extends Controller
             $query = $query->union($query1);
         }
 
-        $userTags = $query->orderBy('skills','desc')->orderBy('answers','desc')->distinct()->simplePaginate(Config::get('api_data_page_size'),'*','page',$page);
+        $userTags = $query->simplePaginate(Config::get('api_data_page_size'),'*','page',$page);
         $data = [];
         foreach($userTags as $userTag){
             if ($question->answers()->where('user_id',$userTag->user_id)->exists()) continue;
