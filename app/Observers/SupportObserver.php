1e7a8577eec9064af807a30a8cc6a1f3b0355490
hank <hank.huiwang@gmail.com>
2018-01-26 11:58:28 +0800

    优化点赞通知

--- app/Observers/SupportObserver.php
+++ app/Observers/SupportObserver.php
@@ -32,28 +32,22 @@ class SupportObserver implements ShouldQueue {
     {
         $source = $support->source;
         $fields = [];
-        switch ($support->supportable_type) {
-            case 'App\Models\Answer':
-                $title = '回答';
-                $fields[] = [
-                    'title' => '回答内容',
-                    'value' => $source->getContentText(),
-                    'short' => false
-                ];
-                $fields[] = [
-                    'title' => '问题地址',
-                    'value' => route('ask.question.detail',['id'=>$source->question_id]),
-                    'short' => false
-                ];
-                //通知，自己除外
-                if ($source->user_id != $support->user_id) {
-                    $source->user->notify(new NewSupport($source->user_id, $support));
-                }
-                //产生一条feed
-                $feed_event = 'question_supported';
-                $feed_target = $source->id.'_'.$support->user_id;
-                $is_feeded = RateLimiter::instance()->getValue($feed_event,$feed_target);
-                if (!$is_feeded) {
+        $title = '';
+        if (RateLimiter::STATUS_GOOD == RateLimiter::instance()->increase('upvote:'.get_class($source),$source->id.'_'.$support->user_id,0)) {
+            switch ($support->supportable_type) {
+                case 'App\Models\Answer':
+                    $title = '回答';
+                    $fields[] = [
+                        'title' => '回答内容',
+                        'value' => $source->getContentText(),
+                        'short' => false
+                    ];
+                    $fields[] = [
+                        'title' => '问题地址',
+                        'value' => route('ask.question.detail',['id'=>$source->question_id]),
+                        'short' => false
+                    ];
+
                     $question = $source->question;
                     if ($question->question_type == 1) {
                         $feed_question_title = '专业回答';
@@ -76,44 +70,8 @@ class SupportObserver implements ShouldQueue {
                             'feed_url'         => $feed_url
                         ])
                         ->log($support->user->name.'赞了'.$feed_question_title, $feed_type);
-                    RateLimiter::instance()->increase($feed_event,$feed_target,3600);
-                }
-                break;
-            case 'App\Models\Submission':
-                $title = '动态';
-                $fields[] = [
-                    'title' => '标题',
-                    'value' => $source->formatTitle()
-                ];
-                $fields[] = [
-                    'title' => '地址',
-                    'value' => config('app.mobile_url').'#/c/'.$source->category_id.'/'.$source->slug
-                ];
-                foreach ($source->data as $field=>$value){
-                    if ($value){
-                        if (is_array($value)) {
-                            foreach ($value as $key => $item) {
-                                $fields[] = [
-                                    'title' => $field.$key,
-                                    'value' => $item
-                                ];
-                            }
-                        } else {
-                            $fields[] = [
-                                'title' => $field,
-                                'value' => $value
-                            ];
-                        }
-                    }
-                }
-                break;
-            default:
-                return;
-        }
-        if (RateLimiter::STATUS_GOOD == RateLimiter::instance()->increase('upvote:'.get_class($source),$source->id.'_'.$support->user_id,0)) {
-            switch ($support->supportable_type) {
-                case 'App\Models\Answer':
-                    $question = $source->question;
+
+
                     event(new Credit($support->user_id,CreditModel::KEY_NEW_UPVOTE,Setting()->get('coins_'.CreditModel::KEY_NEW_UPVOTE),Setting()->get('credits_'.CreditModel::KEY_NEW_UPVOTE),$support->id,'点赞回答'));
                     if ($question->question_type == 1) {
                         event(new Credit($source->user_id,CreditModel::KEY_ANSWER_UPVOTE,Setting()->get('coins_'.CreditModel::KEY_ANSWER_UPVOTE),Setting()->get('credits_'.CreditModel::KEY_ANSWER_UPVOTE),$support->id,'专业回答被点赞'));
@@ -122,20 +80,54 @@ class SupportObserver implements ShouldQueue {
                     }
                     break;
                 case 'App\Models\Submission':
+                    $title = '动态';
+                    $fields[] = [
+                        'title' => '标题',
+                        'value' => $source->formatTitle()
+                    ];
+                    $fields[] = [
+                        'title' => '地址',
+                        'value' => config('app.mobile_url').'#/c/'.$source->category_id.'/'.$source->slug
+                    ];
+                    foreach ($source->data as $field=>$value){
+                        if ($value){
+                            if (is_array($value)) {
+                                foreach ($value as $key => $item) {
+                                    $fields[] = [
+                                        'title' => $field.$key,
+                                        'value' => $item
+                                    ];
+                                }
+                            } else {
+                                $fields[] = [
+                                    'title' => $field,
+                                    'value' => $value
+                                ];
+                            }
+                        }
+                    }
                     event(new Credit($support->user_id,CreditModel::KEY_NEW_UPVOTE,Setting()->get('coins_'.CreditModel::KEY_NEW_UPVOTE),Setting()->get('credits_'.CreditModel::KEY_NEW_UPVOTE),$support->id,'点赞动态分享'));
                     event(new Credit($source->user_id,CreditModel::KEY_READHUB_SUBMISSION_UPVOTE,Setting()->get('coins_'.CreditModel::KEY_READHUB_SUBMISSION_UPVOTE),Setting()->get('credits_'.CreditModel::KEY_READHUB_SUBMISSION_UPVOTE),$support->id,'动态分享被点赞'));
                     break;
+                default:
+                    return;
+                    break;
+            }
+            if ($source->user_id != $support->user_id) {
+                $source->user->notify(new NewSupport($source->user_id,$support));
             }
         }
-
-        return \Slack::to(config('slack.ask_activity_channel'))
-            ->disableMarkdown()
-            ->attach(
-                [
-                    'color'  => 'good',
-                    'fields' => $fields
-                ]
-            )->send('用户'.$support->user->id.'['.$support->user->name.']赞了'.$title);
+        if ($fields) {
+            \Slack::to(config('slack.ask_activity_channel'))
+                ->disableMarkdown()
+                ->attach(
+                    [
+                        'color'  => 'good',
+                        'fields' => $fields
+                    ]
+                )->send('用户'.$support->user->id.'['.$support->user->name.']赞了'.$title);
+        }
+        return;
     }
 
 
