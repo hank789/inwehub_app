77a0eb9cf3bda481a15c200b2174c17b22d42b22
hank <hank.huiwang@gmail.com>
2018-02-01 17:06:23 +0800

    优化通知

--- app/Api/Controllers/SystemController.php
+++ app/Api/Controllers/SystemController.php
@@ -101,8 +101,13 @@ class SystemController extends Controller {
     public function location(Request $request){
         $user = $request->user();
         $clientIp = $request->getClientIp();
+        $iosPushNoticeOpen = $request->input('ios_push_notify');
+        $pushNotify = '';
+        if ($iosPushNoticeOpen != -1) {
+            $pushNotify = 'ios推送:'.($iosPushNoticeOpen?'开启':'关闭');
+        }
         //登陆事件通知
-        event(new UserLoggedIn($user,$request->input('device_system').'唤起'));
+        event(new UserLoggedIn($user,$request->input('device_system').'唤起'.$pushNotify));
         if (RateLimiter::instance()->increase('user-location',$user->id.'-'.$clientIp,3600)) {
             return self::createJsonData(true);
         }
