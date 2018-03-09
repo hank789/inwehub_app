5bdc20250654c4ea4023cb69d193bb34f26cdd35
hank <hank.huiwang@gmail.com>
2018-03-09 17:03:04 +0800

    debug

--- app/Api/Controllers/Weapp/UserController.php
+++ app/Api/Controllers/Weapp/UserController.php
@@ -91,6 +91,7 @@ class UserController extends controller {
             $oauth = $user->userOauth->where('auth_type',UserOauth::AUTH_TYPE_WEAPP)->first();
             $status = $oauth->status;
         } catch (\Exception $e) {
+            \Log::info('test',[$e->getMessage()]);
             $oauth = UserOauth::where('auth_type',UserOauth::AUTH_TYPE_WEAPP)
                 ->where('openid',$request->input('openid'))->first();
             $user = new \stdClass();
