1a5876c4d1e3d8573d7e410e8e162ec223f9f400
hank <hank.huiwang@gmail.com>
2018-04-04 16:58:46 +0800

    优化

--- app/Api/Controllers/NotificationController.php
+++ app/Api/Controllers/NotificationController.php
@@ -70,7 +70,7 @@ class NotificationController extends Controller
             //全部已读
             $im_rooms = Room::where('source_type',User::class)->where(function ($query) use ($user) {$query->where('user_id',$user->id)->orWhere('source_id',$user->id);})->get();
             foreach ($im_rooms as $im_room) {
-                $unreads = MessageRoom::leftJoin('im_messages','message_id','=','im_messages.id')->where('im_message_room.room_id', $im_room->id)->where('im_messages.user_id','!=',$user->id)->whereNull('im_messages.read_at')->get()->pluck('im_messages.id')->toArray();
+                $unreads = MessageRoom::leftJoin('im_messages','message_id','=','im_messages.id')->where('im_message_room.room_id', $im_room->id)->where('im_messages.user_id','!=',$user->id)->whereNull('im_messages.read_at')->select('im_messages.id')->get()->pluck('id')->toArray();
                 if ($unreads) {
                     Message::whereIn('id',$unreads)->update(['read_at' => Carbon::now()]);
                 }
