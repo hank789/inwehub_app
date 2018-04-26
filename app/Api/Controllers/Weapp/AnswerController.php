3aa22198583ea1a22a170644be9c69b554748fb8
hank <hank.huiwang@gmail.com>
2018-04-26 11:51:47 +0800

    优化

--- app/Api/Controllers/Weapp/AnswerController.php
+++ app/Api/Controllers/Weapp/AnswerController.php
@@ -2,6 +2,8 @@
 use App\Api\Controllers\Controller;
 use App\Exceptions\ApiException;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Storage;
 use Tymon\JWTAuth\JWTAuth;
 
 /**
@@ -26,7 +28,27 @@ class AnswerController extends Controller {
             throw new ApiException(ApiException::USER_WEAPP_NEED_REGISTER);
         }
         $description = $request->input('description');
-        return $this->storeAnswer($user,$description,$request);
+        $quillContent = [];
+        $quillContent['ops'][] = [
+            'insert' => $description
+        ];
+        $image_file = 'image_file';
+        if($request->hasFile($image_file)){
+            $file_0 = $request->file($image_file);
+            $extension = strtolower($file_0->getClientOriginalExtension());
+            $extArray = array('png', 'gif', 'jpeg', 'jpg');
+            if(in_array($extension, $extArray)){
+                $file_name = 'quill/'.date('Y').'/'.date('m').'/'.time().str_random(7).'.'.$extension;
+                Storage::disk('oss')->put($file_name,File::get($file_0));
+                $img_url = Storage::disk('oss')->url($file_name);
+                $quillContent['ops'][] = [
+                    'insert' => [
+                        'image' => $img_url
+                    ]
+                ];
+            }
+        }
+        return $this->storeAnswer($user,json_encode($quillContent),$request);
     }
 
 }
