91c13decee80cae464f8425f8b71f70df4b72a69
hank <hank.huiwang@gmail.com>
2018-07-09 11:14:18 +0800

    优化

--- app/Console/Commands/FixData/AddTags.php
+++ app/Console/Commands/FixData/AddTags.php
@@ -41,64 +41,89 @@ class AddTags extends Command
             'sort'      => 0,
             'status'    => 1
         ]);
-        Tag::firstOrCreate([
+        Tag::updateOrCreate([
             'name'          => '新业态',
-            'category_id'   => $category->id
         ],[
             'name'          => '新业态',
             'category_id'   => $category->id
         ]);
-        Tag::firstOrCreate([
+        Tag::updateOrCreate([
+            'name'          => '供应链',
+        ],[
             'name'          => '供应链',
             'category_id'   => $category->id
         ]);
-        Tag::firstOrCreate([
+        Tag::updateOrCreate([
+            'name'          => '企业战略',
+        ],[
             'name'          => '企业战略',
             'category_id'   => $category->id
         ]);
-        Tag::firstOrCreate([
+        Tag::updateOrCreate([
+            'name'          => '数字经济新价值',
+        ],[
             'name'          => '数字经济新价值',
             'category_id'   => $category->id
         ]);
-        Tag::firstOrCreate([
+        Tag::updateOrCreate([
+            'name'          => 'B2B与平台化',
+        ],[
             'name'          => 'B2B与平台化',
             'category_id'   => $category->id
         ]);
-        Tag::firstOrCreate([
+        Tag::updateOrCreate([
+            'name'          => '数字化企业',
+        ],[
             'name'          => '数字化企业',
             'category_id'   => $category->id
         ]);
-        Tag::firstOrCreate([
+        Tag::updateOrCreate([
+            'name'          => '智能制造与工业4.0',
+        ],[
             'name'          => '智能制造与工业4.0',
             'category_id'   => $category->id
         ]);
-        Tag::firstOrCreate([
+        Tag::updateOrCreate([
+            'name'          => '大数据与AI',
+        ],[
             'name'          => '大数据与AI',
             'category_id'   => $category->id
         ]);
-        Tag::firstOrCreate([
+        Tag::updateOrCreate([
+            'name'          => '企业服务',
+        ],[
             'name'          => '企业服务',
             'category_id'   => $category->id
         ]);
-        Tag::firstOrCreate([
+        Tag::updateOrCreate([
+            'name'          => '咨询行业',
+        ],[
             'name'          => '咨询行业',
             'category_id'   => $category->id
         ]);
-        Tag::firstOrCreate([
+        Tag::updateOrCreate([
+            'name'          => 'SAP与Oralce',
+        ],[
             'name'          => 'SAP与Oralce',
             'category_id'   => $category->id
         ]);
-        Tag::firstOrCreate([
+        Tag::updateOrCreate([
+            'name'          => 'SaaS与云',
+        ],[
             'name'          => 'SaaS与云',
             'category_id'   => $category->id
         ]);
-        Tag::firstOrCreate([
+        Tag::updateOrCreate([
+            'name'          => '信息化新技术',
+        ],[
             'name'          => '信息化新技术',
             'category_id'   => $category->id
         ]);
         $tags = Tag::all();
         foreach ($tags as $tag) {
-            TagCategoryRel::firstOrCreate([
+            TagCategoryRel::updateOrCreate([
+                'tag_id' => $tag->id
+            ],[
                 'tag_id' => $tag->id,
                 'category_id' => $tag->category_id
             ]);
