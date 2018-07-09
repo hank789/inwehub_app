8a23aeaeb865e132d767d2da0fa94cdafa5f1955
hank <hank.huiwang@gmail.com>
2018-07-09 10:59:24 +0800

    优化标签

--- app/Console/Commands/FixData/AddTags.php
+++ app/Console/Commands/FixData/AddTags.php
@@ -32,7 +32,7 @@ class AddTags extends Command
      */
     public function handle()
     {
-        $category = Category::create([
+        $category = Category::firstOrCreate(['slug'      => 'region'],[
             'parent_id' => 0,
             'grade'     => 1,
             'name'      => '领域',
@@ -41,61 +41,64 @@ class AddTags extends Command
             'sort'      => 0,
             'status'    => 1
         ]);
-        Tag::create([
+        Tag::firstOrCreate([
+            'name'          => '新业态',
+            'category_id'   => $category->id
+        ],[
             'name'          => '新业态',
             'category_id'   => $category->id
         ]);
-        Tag::create([
+        Tag::firstOrCreate([
             'name'          => '供应链',
             'category_id'   => $category->id
         ]);
-        Tag::create([
+        Tag::firstOrCreate([
             'name'          => '企业战略',
             'category_id'   => $category->id
         ]);
-        Tag::create([
+        Tag::firstOrCreate([
             'name'          => '数字经济新价值',
             'category_id'   => $category->id
         ]);
-        Tag::create([
+        Tag::firstOrCreate([
             'name'          => 'B2B与平台化',
             'category_id'   => $category->id
         ]);
-        Tag::create([
+        Tag::firstOrCreate([
             'name'          => '数字化企业',
             'category_id'   => $category->id
         ]);
-        Tag::create([
+        Tag::firstOrCreate([
             'name'          => '智能制造与工业4.0',
             'category_id'   => $category->id
         ]);
-        Tag::create([
+        Tag::firstOrCreate([
             'name'          => '大数据与AI',
             'category_id'   => $category->id
         ]);
-        Tag::create([
+        Tag::firstOrCreate([
             'name'          => '企业服务',
             'category_id'   => $category->id
         ]);
-        Tag::create([
+        Tag::firstOrCreate([
             'name'          => '咨询行业',
             'category_id'   => $category->id
         ]);
-        Tag::create([
+        Tag::firstOrCreate([
             'name'          => 'SAP与Oralce',
             'category_id'   => $category->id
         ]);
-        Tag::create([
+        Tag::firstOrCreate([
             'name'          => 'SaaS与云',
             'category_id'   => $category->id
         ]);
-        Tag::create([
+        Tag::firstOrCreate([
             'name'          => '信息化新技术',
             'category_id'   => $category->id
         ]);
         $tags = Tag::all();
         foreach ($tags as $tag) {
-            TagCategoryRel::create([
+            TagCategoryRel::firstOrCreate([
                 'tag_id' => $tag->id,
                 'category_id' => $tag->category_id
             ]);
