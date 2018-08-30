f32f26dd94f8244a4df10dc11e5083b3d76dde69
hank <hank.huiwang@gmail.com>
2018-08-30 11:20:15 +0800

    update

--- app/Api/Controllers/Account/ProfileController.php
+++ app/Api/Controllers/Account/ProfileController.php
@@ -104,7 +104,12 @@ class ProfileController extends Controller
             }
         }
 
-        $info['followers'] = $user->followers()->count();
+        $info['followers'] = $user->attentions()->count();
+        $info['publishes'] = $user->userData->questions
+            + $user->userData->answers + Submission::where('user_id',$user->id)->count()
+            + Comment::where('user_id',$user->id)->count();
+        $info['collections'] = $user->collections()->count();
+        $info['groups'] = GroupMember::where('user_id',$user->id)->count();
         $info['feedbacks'] = Feedback::where('to_user_id',$user->id)->count();
         $info['total_score'] = '综合评分暂无';
 
