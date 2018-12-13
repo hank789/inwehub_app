12f1de957d06faa62b04ecea56d70b00112b3443
hank <hank.huiwang@gmail.com>
2018-12-13 11:49:39 +0800

    优化sitemap生成

--- config/sitemap.php
+++ config/sitemap.php
@@ -7,7 +7,7 @@ return [
     'cache_duration' => 3600,
     'escaping' => true,
     'use_limit_size' => false,
-    'max_size' => null,
+    'max_size' => 10000,
     'use_styles' => false,
     'styles_location' => '/vendor/sitemap/styles/',
     'use_gzip' => false
