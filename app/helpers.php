3859024b20a2a26b77c0c77c643b7b85b7fc8d81
hank <hank.huiwang@gmail.com>
2019-01-12 11:32:16 +0800

    去除不必要的告警

--- app/helpers.php
+++ app/helpers.php
@@ -1216,7 +1216,7 @@ if (!function_exists('getUrlInfo')) {
                 }
                 if (!$image) {
                     $img_url = 'https://cdn.inwehub.com/system/group_18@3x.png';
-                    event(new \App\Events\Frontend\System\ExceptionNotify('未取到网站:'.$url.'的图片'));
+                    //event(new \App\Events\Frontend\System\ExceptionNotify('未取到网站:'.$url.'的图片'));
                 }
                 $title = $ql->find('title')->eq(0)->text();
                 if (str_contains($image,'.ico')) {
