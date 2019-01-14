9a64ba4ab85da3c67bea2e91d64d4518bf030319
hank <hank.huiwang@gmail.com>
2019-01-14 17:46:14 +0800

    每日入库文章统计

--- app/Console/Commands/Crontab/DailyRecommendReport.php
+++ app/Console/Commands/Crontab/DailyRecommendReport.php
@@ -6,7 +6,10 @@
  */
 
 use App\Events\Frontend\System\OperationNotify;
+use App\Models\Category;
 use App\Models\RecommendRead;
+use App\Models\Scraper\WechatWenzhangInfo;
+use App\Models\Submission;
 use App\Models\User;
 use Illuminate\Console\Command;
 
@@ -36,6 +39,13 @@ class DailyRecommendReport extends Command
        $recommends = RecommendRead::where('audit_status',1)->whereBetween('created_at',[date('Y-m-d 00:00:00'),date('Y-m-d 23:59:59')])->count();
        $percent = bcadd($recommends/50 * 100,0,2);
        event(new OperationNotify('今日推荐完成率：'.$percent.'%（'.$recommends.'/50）'));
+       $rssCount = WechatWenzhangInfo::where('source_type',2)->where('status',2)->where('date_time','>=',date('Y-m-d 00:00:00'))->count();
+       $wechatCount = WechatWenzhangInfo::where('source_type',1)->where('status',2)->where('date_time','>=',date('Y-m-d 00:00:00'))->count();
+       $category = Category::where('slug','wallstreetcn')->first();
+       $wallstreetCount = Submission::where('category_id',$category->id)->where('created_at','>=',date('Y-m-d 00:00:00'))->count();
+       $category2 = Category::where('slug','sap_blog')->first();
+       $sapCount = Submission::where('category_id',$category2->id)->where('created_at','>=',date('Y-m-d 00:00:00'))->count();
+       event(new OperationNotify('今日截止目前入库文章：'.$rssCount.'(rss);'.$wechatCount.'(微信公众号);'.$wallstreetCount.'(华尔街见闻);'.$sapCount.'(sap blog)'));
     }
 
 }
