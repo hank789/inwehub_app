18deb90de67ff956ba535ddaaf33e0b9a345d81b
hank <hank.huiwang@gmail.com>
2019-04-03 16:48:41 +0800

    增加对google新闻的统计报告

--- app/Console/Commands/Crontab/DailyRecommendReport.php
+++ app/Console/Commands/Crontab/DailyRecommendReport.php
@@ -39,13 +39,16 @@ class DailyRecommendReport extends Command
        $recommends = RecommendRead::where('audit_status',1)->whereBetween('created_at',[date('Y-m-d 00:00:00'),date('Y-m-d 23:59:59')])->count();
        $percent = bcadd($recommends/50 * 100,0,2);
        event(new OperationNotify('今日推荐完成率：'.$percent.'%（'.$recommends.'/50）'));
-       $rssCount = WechatWenzhangInfo::where('source_type',2)->where('status',2)->where('date_time','>=',date('Y-m-d 00:00:00'))->count();
-       $wechatCount = WechatWenzhangInfo::where('source_type',1)->where('status',2)->where('date_time','>=',date('Y-m-d 00:00:00'))->count();
+       $rssCount = WechatWenzhangInfo::where('source_type',2)->where('date_time','>=',date('Y-m-d 00:00:00'))->count();
+       $wechatCount = WechatWenzhangInfo::where('source_type',1)->where('date_time','>=',date('Y-m-d 00:00:00'))->count();
        $category = Category::where('slug','wallstreetcn')->first();
        $wallstreetCount = Submission::where('category_id',$category->id)->where('created_at','>=',date('Y-m-d 00:00:00'))->count();
        $category2 = Category::where('slug','sap_blog')->first();
        $sapCount = Submission::where('category_id',$category2->id)->where('created_at','>=',date('Y-m-d 00:00:00'))->count();
-       event(new OperationNotify('今日截止目前入库文章：'.$rssCount.'(rss);'.$wechatCount.'(微信公众号);'.$wallstreetCount.'(华尔街见闻);'.$sapCount.'(sap blog)'));
+
+       $category3 = Category::where('slug','channel_googlenews')->first();
+       $googleNewsCount = Submission::where('category_id',$category3->id)->where('created_at','>=',date('Y-m-d 00:00:00'))->count();
+       event(new OperationNotify('今日截止目前入库文章：'.$rssCount.'(rss);'.$wechatCount.'(微信公众号);'.$wallstreetCount.'(华尔街见闻);'.$sapCount.'(sap blog);'.$googleNewsCount.'(google新闻)'));
     }
 
 }
