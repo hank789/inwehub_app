43335918a7f02f7d650437764852a741fbd9d757
hank <hank.huiwang@gmail.com>
2019-07-09 11:55:43 +0800

    优化rss抓取

--- app/Console/Commands/Scraper/RssPosts.php
+++ app/Console/Commands/Scraper/RssPosts.php
@@ -58,6 +58,7 @@ class RssPosts extends Command
                 event(new ExceptionNotify('RSS抓取失败：'.$topic->source_link));
                 continue;
             }
+
             foreach ($xml->channel->item as $key => $value) {
                 $image_url    = '';
                 $author       = '';
@@ -66,35 +67,39 @@ class RssPosts extends Command
                 if (strtotime($value->pubDate) <= strtotime('-7 days')) {
                     continue;
                 }
-                $dom = new Dom();
-                $dom->load($value->description);
 
                 $author = $value->author;
                 if (strlen($author) === 0) {
                     $author = $value->children('dc', true)->creator;
                 }
-
-                $img_tags = $dom->find('img');
-                foreach ($img_tags as $img) {
-                    $image_url = $img->getAttribute('src');
-                    // 如果图片链接为空，则跳过下方的处理
-                    if (strlen($image_url) === 0) {
-                        continue;
+                try {
+                    $dom = new Dom();
+                    $dom->load($value->description);
+                    $img_tags = $dom->find('img');
+                    foreach ($img_tags as $img) {
+                        $image_url = $img->getAttribute('src');
+                        // 如果图片链接为空，则跳过下方的处理
+                        if (strlen($image_url) === 0) {
+                            continue;
+                        }
+                        // 省略嵌入式资源处理
+                        if (0 === strpos($image_url, '//')) {
+                            $image_url = 'http:' . $image_url;
+                        }
+                        // 判断是否为 .jpg、.jpeg、.png、.bmp 类型的图片，如果不是则跳过
+                        if (strpos($image_url, '.jpg')  > 0 ||
+                            strpos($image_url, '.jpeg') > 0 ||
+                            strpos($image_url, '.png')  > 0 ||
+                            strpos($image_url, '.bmp')  > 0) {
+                            break;
+                        } else {
+                            $image_url = '';
+                        }
                     }
-                    // 省略嵌入式资源处理
-                    if (0 === strpos($image_url, '//')) {
-                        $image_url = 'http:' . $image_url;
-                    }
-                    // 判断是否为 .jpg、.jpeg、.png、.bmp 类型的图片，如果不是则跳过
-                    if (strpos($image_url, '.jpg')  > 0 ||
-                        strpos($image_url, '.jpeg') > 0 ||
-                        strpos($image_url, '.png')  > 0 ||
-                        strpos($image_url, '.bmp')  > 0) {
-                        break;
-                    } else {
-                        $image_url = '';
-                    }
+                } catch (\Exception $e) {
+                    $this->warn($e->getMessage());
                 }
+
                 // 获取文章的标签
                 /*$category_tags = $value->category;
                 foreach ($category_tags as $category) {
